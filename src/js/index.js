a518840ee17d09de4587041a716bffe41f22a3b7
Chuan Miao <chuan137@gmail.com>
2014-12-17 14:56:49 +0100

    browserify + ractive components

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,18 @@
 // index.js
 /*
 */
+
 var $ = require("jquery");
-var _ = require("underscore");
+//var _ = require("underscore");
+var Ractive = require("ractive/ractive.runtime");
+
+var HalloView = Ractive.extend(require("../components/hallo.html"));
+
+
+hallo = new HalloView({
+    el: 'demo',
+    data: {
+        world: 'Chuan'
+    }
+});
+
